8e660b30cefccafd718599f0a1d56eee629a6466
Evan Anderson <evan.k.anderson@gmail.com>
2019-09-02 06:37:24 +0000

    Add backup version of index.js

--- node-server/index.js
+++ node-server/index.js
@@ -1,77 +1,29 @@
 
-var admin = require('firebase-admin');
-var functions = require('firebase-functions');
-var google = require('googleapis');
-var request = require('request-promise-native');
+var admin = require('firebase-admin')
+var functions = require('firebase-functions')
 
-// admin.database.enableLogging(true);
+admin.database.enableLogging(true);
+
+var serviceAccount = require('./key.json')
 
 var mainApp = admin.initializeApp({
-    // Note that the default credentials will need both 
-    // auth/cloud-platform and auth/userinfo.email
-    credential: admin.credential.applicationDefault(),
+    //    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://argent-public.firebaseio.com",
+    projectId: "argent-public"
 }, 'main');
+/*
+admin.initializeApp(functions.config().firebase);
+*/
 
 var db = mainApp.database();
 
-var wakey = db.ref("config/wakey");
+var clientid = db.ref("config/clientid")
+var ref = db.ref("printers")
 
-wakey.transaction(function(data) {
-    console.log("Examining: " + data + " (" + typeof data + ")");
-    if (data == null) {
-        return "wakey";
-    }
-    if (data == "wakey") {
-        return "wakey-wakey";
-    }
-    return;
-}).then(function(result) {
-    console.log((result.committed? "Wrote" : "Failed") + " to update to " +
-		result.snapshot.ref.toString() + ": " + result.snapshot.val());
+clientid.on("value", function(snapshot) {
+    console.log(snapshot.val());
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
 });
-
-config = db.ref("config").once("value");
-
-config.then(function(snapshot) {
-    var c = snapshot.val();
-    if (c.client_id == null || c.client_secret == null) {
-	console.log("Could not read OAuth client config: " + snapshot.val());
-	return;
-    }
-    var oauth2 = new google.auth.OAuth2(c.client_id, c.client_secret);
-
-    var printers = db.ref("printers");
-
-    printers.orderByKey().on("child_added", function(snapshot) {
-	printerInfo = snapshot.val();
-	if (printerInfo == null || printerInfo.refresh == null) {
-	    console.log(snapshot.key + " is missing refresh token: " + JSON.stringify(printerInfo));
-	    return;
-	}
-	oauth2.setCredentials({refresh_token: printerInfo.refresh});
-	access = oauth2.getAccessToken(
-	    function(err, token) {
-		if (err != null || token == null) {
-		    console.log("Unable to fetch access token for " + snapshot.key + ": " + err);
-		    return
-		}
-		list = request({
-		    method: 'POST',
-		    uri: "https://www.google.com/cloudprint/list",
-		    formData: { proxy: "testme" },
-		    headers: {
-			Authorization: "Bearer " + token
-		    }
-		});
-		list.then(function (result) {
-		    console.log("LIST: " + result);
-		});
-	    });
-        console.log("Processing printer " + snapshot.key);
-    });
-});
-
-
-
-// TODO(evan): exit cleanly after the function finishes
+clientid.off("value");
